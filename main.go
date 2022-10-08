b6fc2a72d7b9e65be2e546196ac93bd9e57e6a00
miyama <36231225+hige-dev@users.noreply.github.com>
2022-10-08 15:02:42 +0900

    get users

--- main.go
+++ main.go
@@ -1,18 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "host=localhost user=root dbname=testdb sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+type User struct {
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "test")
+}
+
+func Users(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		var users []User
+		rows, err := Db.Query("SELECT id, name, created_at FROM users")
+		if err != nil {
+			return
+		}
+		for rows.Next() {
+			user := User{}
+
+			if err = rows.Scan(&user.Id, &user.Name, &user.CreatedAt); err != nil {
+				fmt.Fprint(w, err)
+			}
+			users = append(users, user)
+		}
+		if len(users) == 0 {
+			log.Print("user is not exists.")
+		}
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		if err := enc.Encode(&users); err != nil {
+			// fmt.Fprint(w, err)
+			log.Fatal(err)
+		}
+		if _, err := fmt.Fprintf(w, buf.String()); err != nil {
+			fmt.Fprint(w, err)
+		}
+		rows.Close()
+	case "POST":
+		// user作成
+	default:
+		fmt.Fprintf(w, "Unauthorized Method: %+v", r.Method)
+	}
+	return
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
+	mux.HandleFunc("/", Index)
+	mux.HandleFunc("/users", Users)
 
 	server := &http.Server{
 		Addr:    ":8888",
