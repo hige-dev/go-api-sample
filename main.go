1be41253e18637b29933afbdfceb61d2bd193ba5
miyama <36231225+hige-dev@users.noreply.github.com>
2022-10-08 16:00:16 +0900

    refactoring

--- main.go
+++ main.go
@@ -29,47 +29,6 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "test")
-}
-
-func Users(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		var users []User
-		rows, err := Db.Query("SELECT id, name, created_at FROM users")
-		if err != nil {
-			return
-		}
-		for rows.Next() {
-			user := User{}
-
-			if err = rows.Scan(&user.Id, &user.Name, &user.CreatedAt); err != nil {
-				fmt.Fprint(w, err)
-			}
-			users = append(users, user)
-		}
-		if len(users) == 0 {
-			log.Print("user is not exists.")
-		}
-		var buf bytes.Buffer
-		enc := json.NewEncoder(&buf)
-		if err := enc.Encode(&users); err != nil {
-			// fmt.Fprint(w, err)
-			log.Fatal(err)
-		}
-		if _, err := fmt.Fprintf(w, buf.String()); err != nil {
-			fmt.Fprint(w, err)
-		}
-		rows.Close()
-	case "POST":
-		// user作成
-	default:
-		fmt.Fprintf(w, "Unauthorized Method: %+v", r.Method)
-	}
-	return
-}
-
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Index)
@@ -82,4 +41,51 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Printf("error: %+v", err)
 	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "test")
+}
+
+func Users(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		GetUsers(w, r)
+	case "POST":
+		// user作成
+		// jsonをパースしてDBに保存
+		//
+	default:
+		fmt.Fprintf(w, "Unauthorized Method: %+v", r.Method)
+	}
+	return
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	rows, err := Db.Query("SELECT id, name, created_at FROM users")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		user := User{}
+
+		if err = rows.Scan(&user.Id, &user.Name, &user.CreatedAt); err != nil {
+			fmt.Fprint(w, err)
+		}
+		users = append(users, user)
+	}
+	if len(users) == 0 {
+		log.Print("user is not exists.")
+	}
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&users); err != nil {
+		// fmt.Fprint(w, err)
+		log.Fatal(err)
+	}
+	if _, err := fmt.Fprintf(w, buf.String()); err != nil {
+		fmt.Fprint(w, err)
+	}
+	rows.Close()
 }
